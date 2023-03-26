7ef6689f291d3f38421026235b32605ecd4e7e95
Snow37 <110422108+Snow37@users.noreply.github.com>
2023-03-26 21:25:22 +0300

    Update model.

--- Currency/Models/Currency.swift
+++ Currency/Models/Currency.swift
@@ -9,7 +9,9 @@ import Foundation
 
 struct Currency: Codable {
     let id: String
+    let symbol: String
     let name: String
+    let image: URL
     let currentPrice: Double
     let priceChange: Double
     let priceChangePercentage: Double
@@ -19,7 +21,9 @@ struct Currency: Codable {
 extension Currency {
     enum CodingKeys: String, CodingKey {
         case id = "id"
+        case symbol = "symbol"
         case name = "name"
+        case image = "image"
         case currentPrice = "current_price"
         case priceChange = "price_change_24h"
         case priceChangePercentage = "price_change_percentage_24h"
