b04c77269b6ae1ef175b0feabbfc52cf17198562
Snow37 <110422108+Snow37@users.noreply.github.com>
2023-03-28 00:22:29 +0300

    Recoding model, added int.

--- Currency/Models/Currency.swift
+++ Currency/Models/Currency.swift
@@ -16,6 +16,41 @@ struct Currency: Codable {
     let priceChange: Double
     let priceChangePercentage: Double
     let lastUpdate: String
+    
+    init(id: String,
+         symbol: String,
+         name: String,
+         image: URL,
+         currentPrice: Double,
+         priceChange: Double,
+         priceChangePercentage: Double,
+         lastUpdate: String
+    ) {
+        self.id = id
+        self.symbol = symbol
+        self.name = name
+        self.image = image
+        self.currentPrice = currentPrice
+        self.priceChange = priceChange
+        self.priceChangePercentage = priceChangePercentage
+        self.lastUpdate = lastUpdate
+    }
+    
+    init(currencyDatum: [String: Any]) {
+    id = currencyDatum["id"] as? String ?? ""
+    symbol = currencyDatum["symbol"] as? String ?? ""
+    name = currencyDatum["name"] as? String ?? ""
+    image = currencyDatum["image"] as? URL ?? URL(string: "")!
+    currentPrice = currencyDatum["current_price"] as? Double ?? 0
+    priceChange = currencyDatum["price_change_24h"] as? Double ?? 0
+    priceChangePercentage = currencyDatum["rice_change_percentage_24h"] as? Double ?? 0
+    lastUpdate = currencyDatum["last_updated"] as? String ?? ""
+    }
+    
+    static func getCurrency(from value: Any) -> [Currency] {
+        guard let currencyData = value as? [[String: Any]] else { return [] }
+        return currencyData.map { Currency(currencyDatum: $0)}
+    }
 }
 
 extension Currency {
